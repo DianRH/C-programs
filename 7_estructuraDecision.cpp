df71384ee5e6607eb8905a4fe75487443da78d73
Diana Reyes <diana__rh@hotmail.com>
2018-11-04 21:27:38 -0600

    Estructura de decisión con mayor y menor que

---
+++ 7_estructuraDecision.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a,b;
+	int mayor, menor;
+	
+	printf("Ingrese dos valores: ");
+	scanf("%d %d",&a,&b);
+	
+	if(a>b){
+		
+		mayor = a;
+		menor = b;
+		
+	}else{
+	
+	
+		mayor = b;
+		menor = a;
+	}
+	
+	printf("Mayor: %d\n",mayor);
+	printf("Menor: %d",menor);
+	
+	return 0;
+}
+

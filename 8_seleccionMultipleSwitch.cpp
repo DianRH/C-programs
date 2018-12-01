cd7dbcc3d620b2f944349c454ca31c94a6aed8e4
Diana Reyes <diana__rh@hotmail.com>
2018-12-01 00:10:20 -0600

    switch
    imprime el día de la semana de acuerdo con el numero del case

--- 8_seleccionMultipleSwitch.cpp
+++ 8_seleccionMultipleSwitch.cpp
@@ -1,7 +1,41 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 
 main(){
 	
+	int nroDia;
+	char dia [10];
+	
+	printf("Ingrese dia de la semana: ");
+	scanf("%d",&nroDia);
+	
+	switch(nroDia){
+		
+		case 1:
+			strcpy(dia,"Lunes");
+			break;
+		case 2:
+			strcpy(dia,"Martes");
+			break;
+		case 3:
+			strcpy(dia,"Miercoles");
+			break;
+		case 4:
+			strcpy(dia,"Jueves");
+			break;
+		case 5:
+			strcpy(dia,"Viernes");
+			break;
+		case 6:
+			strcpy(dia,"Sabado");
+			break;
+		case 7:
+			strcpy(dia,"Domingo");
+			break;
+	}
+	
+	printf("%d es %s\n",nroDia,dia);
 	
 	return 0;
 }

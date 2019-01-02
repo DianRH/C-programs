1675b8cd5280122af2161c3b91f8ce77ea1b3f6b
Diana Reyes <diana__rh@hotmail.com>
2019-01-02 00:32:34 -0600

    convertir datos de un tipo a otro (type casting)

---
+++ 10_promedioWhile.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+/*	int n,cant,suma;
+	double prom;
+	
+	printf("Ingrese un valor numerico: ");
+	scanf("%d",&n);
+	
+	cant = 0;
+	suma = 0;
+	
+	while (n!=0)
+	{
+		suma = suma+n;
+		cant = cant+1;
+		
+		printf("Ingrese el siguiente valor : ");
+		scanf("%d",&n);
+		
+	}
+	prom = (double)suma/cant;
+	
+	printf("El promedio es : %lf\n",prom);*/
+	
+	/*-------------------------------------------*/
+	
+	int  a, b;
+	double div;
+	
+	printf("Ingrese dos valores para la division : ");
+	scanf("%d %d",&a,&b);
+	
+	div = (double)a/b;
+	
+		printf("El resultado es: %.2lf \n",div);
+	
+	 
+	
+	return 0;
+}
